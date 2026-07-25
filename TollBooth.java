874b1041d60b14b82d917cc5c2c7593573601a1e
PAMBHARHETVI <pambharchandresh8@gmail.com>
2026-07-25 16:55:29 +0530

    Practical-2

---
+++ TollBooth.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+record Vehicle(String number, String type) {}
+
+public class TollBooth {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int total = 0;
+
+        int bike = 0;
+        int car = 0;
+        int truck = 0;
+
+        while (true) {
+
+            System.out.print("Vehicle number (or done): ");
+            String number = sc.next();
+
+            if (number.equalsIgnoreCase("done")) {
+                break;
+            }
+
+            System.out.print("Type (bike/car/truck): ");
+            String type = sc.next().toLowerCase();
+
+            Vehicle v = new Vehicle(number, type);
+
+            int toll = switch (v.type()) {
+                case "bike" -> 20;
+                case "car" -> 50;
+                case "truck" -> 150;
+                default -> 0;
+            };
+
+            total += toll;
+
+            switch (v.type()) {
+                case "bike" -> bike++;
+                case "car" -> car++;
+                case "truck" -> truck++;
+            }
+
+            System.out.println(v.number() + " pays " + toll);
+        }
+
+        System.out.println("Total toll: " + total);
+
+        if (bike >= car && bike >= truck)
+            System.out.println("Most frequent: bike");
+        else if (car >= bike && car >= truck)
+            System.out.println("Most frequent: car");
+        else
+            System.out.println("Most frequent: truck");
+
+       
+    }
+}
