f856ba074b79f91edce4ea919dc104ea1cc007fb
PAMBHARHETVI <pambharchandresh8@gmail.com>
2026-07-27 16:11:13 +0530

    Practical-4

---
+++ Password Strength/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+    public static void main(String args[])
+    {
+        String password="Password@123";
+        Password pwd = new Password();
+        pwd.checkPassword(password);
+    }
+}
